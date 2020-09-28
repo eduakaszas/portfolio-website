fbfc1ca9832d64537cbdfbca789fbbbdb38452f9
Edua Kaszas <edua.kaszas@usabilla.com>
2020-09-28 22:50:32 +0200

    Created basic hamburger menu and nav animation

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -7,15 +7,15 @@ function NavigationBar() {
             <h3 className="website-logo"> Cool Logo </h3>
 
             <input type="checkbox" id="nav-toggle" className="nav-toggle" />
+            <label for="nav-toggle" class="nav-toggle-label">
+                <span></span>
+            </label>
             <nav>
                 <ul>
                     <li><a href="#"> Projects </a></li>
                     <li><a href="#"> About </a></li>
                 </ul>
             </nav>
-            {/* <label for="nav-toggle" class="nav-toggle-label">
-                <span></span>
-            </label> */}
         </div>
     )
 }
