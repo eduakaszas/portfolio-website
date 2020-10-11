21fbef79f9032bbdde4fb017728a2c971c7fdbd2
eduakaszas <33251564+eduakaszas@users.noreply.github.com>
2020-10-11 19:19:01 +0200

    Interactive canvas background implementation (#2)
    * Added canvas as background that one can draw on
    
    * Fixed position of paint pots
    
    Co-authored-by: Edua Kaszas <edua.kaszas@usabilla.com>

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
-import NavigationBar from './components/NavigationBar/NavigationBar'
-import EntryScreen from './components/EntryScreen/EntryScreen';
-import Projects from './components/Projects/Projects';
-import AboutMe from './components/AboutMe/AboutMe';
+import NavigationBar from './components/NavigationBar/NavigationBar';
+// import EntryScreen from './components/EntryScreen/EntryScreen';
+// import Projects from './components/Projects/Projects';
+// import AboutMe from './components/AboutMe/AboutMe';
+import CanvasBackground from './components/CanvasBackground/CanvasBackground';
 
-function App() {
-	return (
-		<div className="container">
-			<NavigationBar />
-			{/* <EntryScreen />
-			<Projects />
-			<AboutMe /> */}
-		</div>
-	);
-};
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			pickedColor: null
+		}
+	}
+
+	selectColor = e => {
+        const selectedColor = e.target.style.backgroundColor;
+		console.log( selectedColor )
+		
+        this.setState({
+            pickedColor: selectedColor
+        })
+    }
+
+	render() {
+		const browserWidth = window.innerWidth;
+		const { pickedColor } = this.state;
+
+		return (
+			<div className="container">
+				{ browserWidth >= 1000
+					? <CanvasBackground pickedColor={ pickedColor }
+										selectColor={ this.selectColor } 
+					/>
+					: null
+				}
+				<NavigationBar />
+				{/* <EntryScreen />
+				<Projects />
+				<AboutMe /> */}
+			</div>
+		)
+	}
+}
 
 export default App;
---
+++ src/components/CanvasBackground/CanvasBackground.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import PaintPots from '../PaintPots/PaintPots.js';
+
+
+import './CanvasBackground.scss';
+
+class CanvasBackground extends Component {
+	constructor(props) {
+		super(props);
+		this.canvasRef = React.createRef();
+        this.contextRef = React.createRef();
+        
+        this.state = {
+            isDrawing: false
+		}
+    }
+    
+    componentDidMount() {
+        const canvas = this.canvasRef.current;
+        canvas.width = window.innerWidth * 2;
+        canvas.height = window.innerHeight * 2;
+        canvas.style.width = `${ window.innerWidth }px`;
+        canvas.style.height = `${ window.innerHeight }px`;
+
+        const context = canvas.getContext("2d");
+        context.scale(2,2);
+        context.lineCap = "round";
+        context.strokeStyle = "black";
+        context.lineWidth = 5;
+        this.contextRef.current = context;
+    }
+
+    startDrawing = ({nativeEvent}) => {
+        const { pickedColor } = this.props;
+        const { offsetX, offsetY } = nativeEvent;
+        const currentContextRef = this.contextRef.current;
+
+        currentContextRef.beginPath();
+        currentContextRef.moveTo(offsetX, offsetY);
+        currentContextRef.strokeStyle = pickedColor;
+        
+        this.setState({
+            isDrawing: true
+        })
+	}
+
+	finishDrawing = () => {
+        const currentContextRef = this.contextRef.current;
+        currentContextRef.closePath();
+
+        this.setState({
+            isDrawing: false
+        })
+	}
+
+	draw = ({nativeEvent}) => {
+        const { isDrawing } = this.state;
+        if( !isDrawing ) return;
+
+        const { offsetX, offsetY } = nativeEvent;
+        const currentContextRef = this.contextRef.current;
+
+        currentContextRef.lineTo(offsetX, offsetY);
+        currentContextRef.stroke();
+    }
+
+    // clearCanvas = () => {
+    //     const currentContextRef = this.contextRef.current;
+    //     const canvas = this.canvasRef.current;
+    //     canvas.width = window.innerWidth * 2;
+    //     canvas.height = window.innerHeight * 2;
+
+    //     currentContextRef.clearRect(0, 0, currentContextRef.canvas.width, currentContextRef.canvas.height);
+    // }
+
+    render() {
+        const { selectColor } = this.props;
+
+        return (
+            <div>
+                <canvas 
+                    onMouseDown={ this.startDrawing }
+                    onMouseUp={ this.finishDrawing }
+                    onMouseMove={ this.draw }
+                    ref={ this.canvasRef }
+                />
+                <PaintPots selectColor={ selectColor }
+                            // clearCanvas={ this.clearCanvas }
+                />
+            </div>
+        )
+    }
+}
+
+export default CanvasBackground;
