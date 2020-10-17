14683b7f9913ba7bbfaa70ad4644218abde2d4cd
Edua Kaszas <edua.kaszas@usabilla.com>
2020-10-17 22:46:16 +0200

    Made draw-mode switch functional

--- src/components/DesktopMenu/DesktopMenu.js
+++ src/components/DesktopMenu/DesktopMenu.js
@@ -2,11 +2,16 @@ import React from 'react';
 import CanvasModeSwitch from '../CanvasModeSwitch/CanvasModeSwitch.js';
 import './DesktopMenu.scss';
 
-function DesktopMenu() {
+function DesktopMenu(props) {
     return (
         <nav className="navbar">
             <ul className="nav-item-list">
-                <li className="switch"><CanvasModeSwitch /></li>
+                <li className="switch">
+                    <CanvasModeSwitch 
+                        switchDrawMode={ props.switchDrawMode } 
+                        isDrawModeOn={ props.isDrawModeOn }
+                    />
+                </li>
                 <li className="draw-mode"> Draw mode </li>
                 <li><a href="#" className="desktop-menu-item"> Projects </a></li>
                 <li><a href="#" className="desktop-menu-item"> About </a></li>
