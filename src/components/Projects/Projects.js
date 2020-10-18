a54b59e386fd06a14b998b2195aa65765eda5866
Edua Kaszas <edua.kaszas@usabilla.com>
2020-10-18 21:38:13 +0200

    Added some filler text and made alignment changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,12 +4,21 @@ import './Projects.scss';
 function Projects() {
     return (
         <ul className="projects-list">
-            <li className="project image"> Image </li>
-            <li className="project text"> Text </li>
-            <li className="project text"> Text </li>
-            <li className="project image"> Image </li>
-            <li className="project image"> Image </li>
-            <li className="project text"> Text </li>
+            <li className="project image left"> Image </li>
+            <li className="project text right">
+                <h3> Mood Journal </h3>
+                <p> An app to log your mood and activities and help find patterns. </p>
+            </li>
+            <li className="project text left">
+                <h3> Sudoku game </h3>
+                <p> Sudoku for all ages! </p>
+            </li>
+            <li className="project image right"> Image </li>
+            <li className="project image left"> Image </li>
+            <li className="project text right">
+                <h3> This website </h3>
+                <p> A personal website showcasing my projects and with a fun twist! </p>
+            </li>
         </ul>
     )
 }
