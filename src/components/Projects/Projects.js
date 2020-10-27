b10d79422d1ff68aca73520139346e624c442d87
Edua Kaszas <edua.kaszas@usabilla.com>
2020-10-27 22:24:21 +0100

    Create grid layout for the Projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,20 +4,26 @@ import './Projects.scss';
 function Projects() {
     return (
         <ul className="projects-list">
-            <li className="project image left"> Image </li>
-            <li className="project text right">
-                <h3> Mood Journal </h3>
-                <p> An app to log your mood and activities and help find patterns. </p>
+            <li className="project">
+                <div className="image mood-img"></div>
+                <div className="text mood-text">
+                    <h3> Mood Journal </h3>
+                    <p> An app to log your mood and activities and help find patterns. </p>
+                </div>
             </li>
-            <li className="project image right"> Image </li>
-            <li className="project text left">
-                <h3> Sudoku game </h3>
-                <p> Sudoku for all ages! </p>
+            <li className="project">
+                <div className="image sudoku-img"></div>
+                <div className="text sudoku-text">
+                    <h3> Sudoku game </h3>
+                    <p> Sudoku for all ages! </p>
+                </div>
             </li>
-            <li className="project image left"> Image </li>
-            <li className="project text right">
-                <h3> This website </h3>
-                <p> A personal website showcasing my projects and with a fun twist! </p>
+            <li className="project">
+                <div className="image site-img"></div>
+                <div className="text site-text">
+                    <h3> This website </h3>
+                    <p> A personal website showcasing my projects and with a fun twist! </p>
+                </div>
             </li>
         </ul>
     )
